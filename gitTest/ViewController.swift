d227e3fb3e8b967497826f2eed61a4df0f7f25c9
Semih-C <semih_can-b@hotmail.com>
2020-04-18 15:07:01 +0300

    Initial Commit

---
+++ gitTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  gitTest
+//
+//  Created by Semih CAN on 18.04.2020.
+//  Copyright © 2020 Semih CAN. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
